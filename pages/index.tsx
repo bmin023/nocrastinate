cca8e7c00b24d2ffd83d3d84a448b3493eaed375
Bminor023 <bminor230@gmail.com>
2022-02-25 16:11:53 -0600

    Added some basic code for connecting to mysql

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Wacode 2022</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
-          <span className="text-blue-600">
+          <span className="from-blue-600 to-red-400 bg-gradient-to-r text-transparent bg-clip-text">
             WACODE 2022
           </span>
         </h1>
