25ddf3c83b35f127f62465fe50e0bcec9d83f4f2
Bminor023 <bminor230@gmail.com>
2022-02-26 14:23:02 -0600

    Start of types

--- types/schoolTypes.ts
+++ types/schoolTypes.ts
@@ -1,9 +1,24 @@
 export type User = {
   name: string;
   preferences: Preferences;
+  tasks: Task[];
 };
+
 export type Preferences = {
 
-}  
-export type Task = {
+}
+
+export type Schedule = {
   
+}
+
+export type Task = {
+  name: string;
+  dueDate: Date;
+  priority?: "high" | "medium" | "low";
+  estTimeRequired?: number;
+  class?: string;
+  subject?: string;
+  pinned?: boolean;
+  completed?: boolean;
+};
