4fb723819bd350b3860cc590598886b78fd198b4
Bminor023 <bminor230@gmail.com>
2022-02-27 09:15:13 -0600

    getFixedWeek

--- utils/classUtils.ts
+++ utils/classUtils.ts
@@ -1,51 +1,75 @@
-import { User, Plan } from "../types/schoolTypes";
-import moment, { Moment } from "moment";
+import { User, Plan } from '../types/schoolTypes'
+import moment, { Moment } from 'moment'
 
-const DATE_FORMAT = "ddd MMM DD h:mm:ss a";
+const DATE_FORMAT = 'ddd MMM DD h:mm:ss a'
 
 export const getWeek = (user: User, numWeeksFromNow: number) => {
-  let plans = [];
-  const currentDate = moment();
+  let plans = []
+  const currentDate = moment()
 
-  let weekStart = currentDate.clone().startOf('isoWeek').add(numWeeksFromNow, 'weeks');
-  let weekEnd = currentDate.clone().endOf('isoWeek').add(numWeeksFromNow, 'weeks');
+  let weekStart = currentDate
+    .clone()
+    .startOf('isoWeek')
+    .add(numWeeksFromNow, 'weeks')
+  let weekEnd = currentDate
+    .clone()
+    .endOf('isoWeek')
+    .add(numWeeksFromNow, 'weeks')
 
-  const fixedPlans = getFixedWeek(user, weekStart, weekEnd);
-  plans.push(fixedPlans);
+  const fixedPlans = getFixedWeek(user, weekStart, weekEnd)
+  plans.push(fixedPlans)
 
-  console.log(weekStart.format(DATE_FORMAT));
-  console.log(weekEnd.format(DATE_FORMAT));
+  console.log(weekStart.format(DATE_FORMAT))
+  console.log(weekEnd.format(DATE_FORMAT))
 }
 
-const getFixedWeek = (user: User, weekStart: Moment, weekEnd: Moment) => {
-  let plans = [];
+export const getFixedWeek = (
+  user: User,
+  weekStart: Moment,
+  weekEnd: Moment
+) => {
+  let plans = []
 
   for (const plan of user.fixedPlans) {
+    let tPlan = plan
     if (plan.recurring) {
-      let rePlan = plan;
-      moment(rePlan.startTime).subtract(moment(rePlan.startTime).startOf('isoWeek').add(weekStart, 'date'));
-      rePlan.recurring = false;
-      plans.push(rePlan);
+      tPlan.startTime = moment(tPlan.startTime)
+        .add(
+          weekStart.diff(moment(tPlan.startTime).startOf('isoWeek'), 'days'),
+          'days'
+        )
+        .toDate()
+      tPlan.endTime = moment(tPlan.endTime)
+        .add(
+          weekStart.diff(moment(tPlan.endTime).startOf('isoWeek'), 'days'),
+          'days'
+        )
+        .toDate()
+      tPlan.recurring = false
+      plans.push(tPlan)
     } else {
-      // not recurring
+      if (moment(tPlan.startTime).isBetween(weekStart, weekEnd)) {
+        plans.push(tPlan)
+      }
     }
   }
+  return plans
 }
 
 export const splitByWeekday = (schedule: Plan[]): Plan[][] => {
-  const currentDate = moment();
+  const currentDate = moment()
 
-  let weekStart = currentDate.clone().startOf('isoWeek');
-  let weekEnd = currentDate.clone().endOf('isoWeek');
+  let weekStart = currentDate.clone().startOf('isoWeek')
+  let weekEnd = currentDate.clone().endOf('isoWeek')
 
   let weekdays: Plan[][] = [[], [], [], [], [], [], []]
   for (let plan of schedule) {
-    (weekdays.at(moment(plan.startTime).weekday()) as Plan[]).push(plan);
+    ;(weekdays.at(moment(plan.startTime).weekday()) as Plan[]).push(plan)
   }
 
-  return weekdays;
+  return weekdays
 }
 
 export const niceDateString = (date: Date): string => {
-  return moment(date).format(DATE_FORMAT);
+  return moment(date).format(DATE_FORMAT)
 }
